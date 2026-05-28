c35dac378468d1b05b194b27e8f33b992eb19abd
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-05-28 21:21:28 +0530

    modified

--- src/main/java/com/shavindu/springboot_basics/SpringbootBasicsApplication.java
+++ src/main/java/com/shavindu/springboot_basics/SpringbootBasicsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBasicsApplication {
-
+    //Test
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBasicsApplication.class, args);
     }
