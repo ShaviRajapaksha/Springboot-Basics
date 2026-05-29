c220d211242a96558399c97cdc9c6f7879759c64
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-05-29 12:55:08 +0530

    Created StripPaymentService

---
+++ src/main/java/com/shavindu/springboot_basics/StripPaymentService.java
@@ -0,0 +1,10 @@
+package com.shavindu.springboot_basics;
+
+public class StripPaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("STRIPE");
+        System.out.println("Amount" + amount);
+
+    }
+}
