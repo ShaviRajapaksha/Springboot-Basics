4c6ee9e38de115aa3c1730d3b08c5a61709d28c3
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-05-29 12:54:43 +0530

    Created OrderService

---
+++ src/main/java/com/shavindu/springboot_basics/OrderService.java
@@ -0,0 +1,13 @@
+package com.shavindu.springboot_basics;
+
+public class OrderService {
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public void placeOrder () {
+          paymentService.processPayment(10);
+    }
+}
