f79b96238c96b959ee05d1c460d6a679a15ff1b8
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-05-31 13:51:06 +0530

    Base Files

---
+++ spring-boot/src/test/java/com/shavindu/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shavindu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
