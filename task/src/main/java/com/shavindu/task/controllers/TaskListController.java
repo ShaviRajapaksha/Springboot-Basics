5f633670ebf0593b285e8ea35eeb7ddd1e04323c
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-06-04 13:40:42 +0530

    Feat: Create TaskList Controller

---
+++ task/src/main/java/com/shavindu/task/controllers/TaskListController.java
@@ -0,0 +1,34 @@
+package com.shavindu.task.controllers;
+
+import com.shavindu.task.domain.dto.TaskListDto;
+import com.shavindu.task.mappers.TaskListMapper;
+import com.shavindu.task.services.TaskListService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/task-lists")
+public class TaskListController {
+
+    private final TaskListService taskListService;
+    private final TaskListMapper taskListMapper;
+
+    public TaskListController(TaskListService taskListService, TaskListMapper taskListMapper) {
+        this.taskListService = taskListService;
+        this.taskListMapper = taskListMapper;
+    }
+
+    @GetMapping
+    public List<TaskListDto> listTaskLists() {
+        return taskListService.listTaskLists()
+                .stream()
+                .map(taskListMapper::toDto)
+                .toList();
+    }
+
+
+
+}
